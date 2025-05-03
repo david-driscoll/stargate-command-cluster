210cf3545a3ca98d0fd8f73319c8de660511cbc1
David Driscoll <david.driscoll@gmail.com>
2025-05-02 22:49:29 -0400

    init the config

---
+++ kubernetes/apps/sgc/home/home-assistant/scripts/check_and_clone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if the /config/.git directory exists
+if [ ! -d "/config/.git" ]; then
+  echo "Git directory not found. Cloning repository..."
+  git clone --bare ${HOME_ASSISTANT_SSH_URL} /config/.git
+else
+  echo "Git directory exists. Hydrating master branch..."
+  git --git-dir=/config/.git --work-tree=/config checkout master
+fi

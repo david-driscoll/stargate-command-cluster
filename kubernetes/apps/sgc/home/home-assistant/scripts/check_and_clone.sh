5d56cfe271bb38b27e588947263ca7a8bb2b84d5
David Driscoll <david.driscoll@gmail.com>
2025-05-08 20:05:01 -0400

    move ingress

--- kubernetes/apps/sgc/home/home-assistant/scripts/check_and_clone.sh
+++ kubernetes/apps/sgc/home/home-assistant/scripts/check_and_clone.sh
@@ -2,9 +2,9 @@
 
 # Check if the /config/.git directory exists
 if [ ! -d "/config/.git" ]; then
-  echo "Git directory not found. Cloning repository..."
-  git clone --bare ${HOME_ASSISTANT_SSH_URL} /config/.git
+    echo "Git directory not found. Cloning repository..."
+    git clone --bare ${HOME_ASSISTANT_SSH_URL} /config/.git
 else
-  echo "Git directory exists. Hydrating master branch..."
-  git --git-dir=/config/.git --work-tree=/config checkout master
+    echo "Git directory exists. Hydrating master branch..."
+    git --git-dir=/config/.git --work-tree=/config checkout master
 fi

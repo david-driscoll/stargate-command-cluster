7148e2b8e9247a8a28157205e47c4cc3c115eafd
David Driscoll <david.driscoll@gmail.com>
2025-05-25 22:24:58 -0400

    lets gooo

--- kubernetes/apps/network/tailscale/authkey/resources/get-access-token.sh
+++ kubernetes/apps/network/tailscale/authkey/resources/get-access-token.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-apt install kubelet kubeadm kubectl -y
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+./chmod +x kubectl
+./kubectl version
 
 authkey=$(go run tailscale.com/cmd/get-authkey@latest -reusable -preauthorized -tags "tag:${CLUSTER_CNAME}")
 
@@ -10,10 +12,10 @@ token_response=$(curl -d "client_id=$OAUTH_CLIENT_ID" -d "client_secret=$OAUTH_C
 access_token=$(echo $token_response | jq -r '.access_token')
 echo "access_token: $access_token"
 
-kubectl create secret generic tailscale-access-token --from-literal=token="$access_token" --dry-run=client -o yaml | kubectl apply -f -
-kubectl annotate secret tailscale-access-token reflector.v1.k8s.emberstack.com/reflection-allowed='true' --dry-run=client -o yaml | kubectl apply -f -
-kubectl annotate secret tailscale-access-token reloader.stakater.com/auto='true' --dry-run=client -o yaml | kubectl apply -f -
+./kubectl create secret generic tailscale-access-token --from-literal=token="$access_token" --dry-run=client -o yaml | ./kubectl apply -f -
+./kubectl annotate secret tailscale-access-token reflector.v1.k8s.emberstack.com/reflection-allowed='true' --dry-run=client -o yaml | ./kubectl apply -f -
+./kubectl annotate secret tailscale-access-token reloader.stakater.com/auto='true' --dry-run=client -o yaml | ./kubectl apply -f -
 
-kubectl create secret generic tailscale-authkey --from-literal=authkey="$authkey" --dry-run=client -o yaml | kubectl apply -f -
-kubectl annotate secret tailscale-authkey reflector.v1.k8s.emberstack.com/reflection-allowed='true' --dry-run=client -o yaml | kubectl apply -f -
-kubectl annotate secret tailscale-authkey reloader.stakater.com/auto='true' --dry-run=client -o yaml | kubectl apply -f -
+./kubectl create secret generic tailscale-authkey --from-literal=authkey="$authkey" --dry-run=client -o yaml | ./kubectl apply -f -
+./kubectl annotate secret tailscale-authkey reflector.v1.k8s.emberstack.com/reflection-allowed='true' --dry-run=client -o yaml | ./kubectl apply -f -
+./kubectl annotate secret tailscale-authkey reloader.stakater.com/auto='true' --dry-run=client -o yaml | ./kubectl apply -f -

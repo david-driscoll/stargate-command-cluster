c357e0ebcb5bf848df8b30fa81b8db08360a47c3
David Driscoll <david.driscoll@gmail.com>
2025-06-15 16:20:21 -0400

    copy to hostname

--- kubernetes/apps/sgc/home/iptv-proxy/resources/minio-fetch-config.sh
+++ kubernetes/apps/sgc/home/iptv-proxy/resources/minio-fetch-config.sh
@@ -8,6 +8,8 @@ export MC_CONFIG_DIR
 mc alias set "$MC_ALIAS" "$MINIO_ENDPOINT" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
 
 # Copy all files from the bucket to /cache
-mc cp --recursive "$MC_ALIAS/$MINIO_BUCKET/*" /cache
+mc cp "$MC_ALIAS/$MINIO_BUCKET/$TIVI_HOSTNAME.m3u" /cache/$TIVI_HOSTNAME.m3u
+mc cp "$MC_ALIAS/$MINIO_BUCKET/$TIVI_HOSTNAME.xml" /cache/$TIVI_HOSTNAME.xml
 
-echo "Files copied from $MINIO_BUCKET to /cache."
+echo "Files copied from $MINIO_BUCKET to /cache/."
+ls -al /cache/

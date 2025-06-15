6357a529fb797c839dd069ec46a9eb4af6652c32
David Driscoll <david.driscoll@gmail.com>
2025-06-15 11:38:59 -0400

    minio client

--- kubernetes/apps/sgc/home/iptv-proxy/resources/minio-fetch-config.sh
+++ kubernetes/apps/sgc/home/iptv-proxy/resources/minio-fetch-config.sh
@@ -8,6 +8,6 @@ export MC_CONFIG_DIR
 mc alias set "$MC_ALIAS" "$MINIO_ENDPOINT" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
 
 # Copy all files from the bucket to /config
-mc cp --recursive "$MC_ALIAS/$MINIO_BUCKET" /config
+mc cp --recursive "$MC_ALIAS/$MINIO_BUCKET/*" /config
 
 echo "Files copied from $MINIO_BUCKET to /config."

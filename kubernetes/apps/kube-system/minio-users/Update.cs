7bdff451f0ce2e92815dcb5b58f54a82d491cba6
David Driscoll <david.driscoll@gmail.com>
2025-06-20 23:04:35 -0400

    manual volsync replicas

--- kubernetes/apps/kube-system/minio-users/Update.cs
+++ kubernetes/apps/kube-system/minio-users/Update.cs
@@ -1,10 +1,10 @@
 #!/usr/bin/dotnet run
 #:package YamlDotNet@16.3.0
 #:package gstocco.YamlDotNet.YamlPath@1.0.26
+#:package System.Collections.Immutable@10.0.0-preview.5.25277.114
 #:package Spectre.Console@0.50.0
 #:package Spectre.Console.Json@0.50.0
 #:package Dumpify@0.6.6
-#:package System.Collections.Immutable@10.0.0-preview.5.25277.114
 
 
 using Spectre.Console;

80bdf08169155fa99666b80e67df9b154c1010bd
David Driscoll <david.driscoll@gmail.com>
2025-07-13 23:30:27 -0400

    bind to internal ip

--- kubernetes/apps/database/garage/users/resources/init-users.sh
+++ kubernetes/apps/database/garage/users/resources/init-users.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 set -e
-curl -L -o /tmp/garage https://garagehq.deuxfleurs.fr/_releases/v2.0.0/x86_64-unknown-linux-musl/garage && chmod +x /tmp/garage
-/tmp/garage key import -n cluster-user "$GARAGE_USER_CLUSTER_USER" "$GARAGE_PASSWORD_CLUSTER_USER"
-/tmp/garage key allow --create-bucket cluster-user
-/tmp/garage bucket allow --read --write --owner cluster-user --key cluster-user
-/tmp/garage key import -n authentik "$GARAGE_USER_AUTHENTIK" "$GARAGE_PASSWORD_AUTHENTIK"
-/tmp/garage bucket create authentik/postgres
-/tmp/garage bucket allow --read --write --owner authentik/postgres --key authentik
-/tmp/garage bucket create iris
-/tmp/garage bucket allow --read --write --owner iris --key authentik
-/tmp/garage bucket website --allow iris
-/tmp/garage key import -n postgres-sgc "$GARAGE_USER_POSTGRES_SGC" "$GARAGE_PASSWORD_POSTGRES_SGC"
-/tmp/garage bucket create postgres
-/tmp/garage bucket allow --read --write --owner postgres --key postgres-sgc
-/tmp/garage key import -n tivi-sync "$GARAGE_USER_TIVI_SYNC" "$GARAGE_PASSWORD_TIVI_SYNC"
-/tmp/garage bucket create tivi-cache
-/tmp/garage bucket allow --read --write --owner tivi-cache --key tivi-sync
-/tmp/garage bucket create tivi-results
-/tmp/garage bucket allow --read --write --owner tivi-results --key tivi-sync
-/tmp/garage bucket website --allow tivi-results
+
+# Set the namespace and pod name for garage
+NAMESPACE="garage"
+POD="garage-0"
+GARAGE_CMD="kubectl exec -n $NAMESPACE $POD -- garage"
+
+$GARAGE_CMD key import -n cluster-user "$GARAGE_USER_CLUSTER_USER" "$GARAGE_PASSWORD_CLUSTER_USER"
+$GARAGE_CMD key allow --create-bucket cluster-user
+$GARAGE_CMD bucket allow --read --write --owner cluster-user --key cluster-user
+$GARAGE_CMD key import -n authentik "$GARAGE_USER_AUTHENTIK" "$GARAGE_PASSWORD_AUTHENTIK"
+$GARAGE_CMD bucket create authentik/postgres
+$GARAGE_CMD bucket allow --read --write --owner authentik/postgres --key authentik
+$GARAGE_CMD bucket create iris
+$GARAGE_CMD bucket allow --read --write --owner iris --key authentik
+$GARAGE_CMD bucket website --allow iris
+$GARAGE_CMD key import -n postgres-sgc "$GARAGE_USER_POSTGRES_SGC" "$GARAGE_PASSWORD_POSTGRES_SGC"
+$GARAGE_CMD bucket create postgres
+$GARAGE_CMD bucket allow --read --write --owner postgres --key postgres-sgc
+$GARAGE_CMD key import -n tivi-sync "$GARAGE_USER_TIVI_SYNC" "$GARAGE_PASSWORD_TIVI_SYNC"
+$GARAGE_CMD bucket create tivi-cache
+$GARAGE_CMD bucket allow --read --write --owner tivi-cache --key tivi-sync
+$GARAGE_CMD bucket create tivi-results
+$GARAGE_CMD bucket allow --read --write --owner tivi-results --key tivi-sync
+$GARAGE_CMD bucket website --allow tivi-results

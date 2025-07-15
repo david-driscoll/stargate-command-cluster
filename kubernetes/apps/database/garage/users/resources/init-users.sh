00ad69615096b14e0a1524b25c808ddaaab39351
David Driscoll <david.driscoll@gmail.com>
2025-07-15 19:30:07 -0400

    Updated garage

--- kubernetes/apps/database/garage/users/resources/init-users.sh
+++ kubernetes/apps/database/garage/users/resources/init-users.sh
@@ -4,7 +4,7 @@ set -e
 # Set the namespace and pod name for garage
 NAMESPACE="database"
 POD="garage-0"
-GARAGE_CMD="kubectl exec -n $NAMESPACE $POD -- garage"
+GARAGE_CMD="kubectl exec -n $NAMESPACE $POD -- ./garage"
 
 $GARAGE_CMD key import -n cluster-user "$GARAGE_USER_CLUSTER_USER" "$GARAGE_PASSWORD_CLUSTER_USER"
 $GARAGE_CMD key allow --create-bucket cluster-user

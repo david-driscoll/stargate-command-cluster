9e4d9b86fbfabd4fb50c95dde45f6f9623847f14
David Driscoll <david.driscoll@gmail.com>
2025-07-16 10:53:56 -0400

    postgres

--- kubernetes/apps/database/garage/users/resources/init-users.sh
+++ kubernetes/apps/database/garage/users/resources/init-users.sh
@@ -15,6 +15,10 @@ $GARAGE_CMD bucket create iris || true
 $GARAGE_CMD bucket allow --read --write --owner iris --key authentik
 $GARAGE_CMD bucket allow --read --write iris --key cluster-user
 $GARAGE_CMD bucket website --allow iris
+$GARAGE_CMD key import -n postgres-sgc --yes "$GARAGE_USER_POSTGRES_SGC" "$GARAGE_PASSWORD_POSTGRES_SGC" || true
+$GARAGE_CMD bucket create postgres || true
+$GARAGE_CMD bucket allow --read --write --owner postgres --key postgres-sgc
+$GARAGE_CMD bucket allow --read --write postgres --key cluster-user
 $GARAGE_CMD key import -n tivi-sync --yes "$GARAGE_USER_TIVI_SYNC" "$GARAGE_PASSWORD_TIVI_SYNC" || true
 $GARAGE_CMD bucket create tivi-cache || true
 $GARAGE_CMD bucket allow --read --write --owner tivi-cache --key tivi-sync

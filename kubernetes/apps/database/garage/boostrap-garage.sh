21e1c3b9c7628f1811a2a19418f947da576bf253
David Driscoll <david.driscoll@gmail.com>
2025-07-16 20:44:47 -0400

    disable things

--- kubernetes/apps/database/garage/boostrap-garage.sh
+++ kubernetes/apps/database/garage/boostrap-garage.sh
@@ -1,4 +1,6 @@
 kubectl exec --stdin --tty -n database garage-0 -- ./garage status
-kubectl exec --stdin --tty -n database garage-0 -- ./garage layout assign xyz -z zero -c 64G
-kubectl exec --stdin --tty -n database garage-0 -- ./garage layout show
-kubectl exec --stdin --tty -n database garage-0 -- ./garage layout apply --version 1
+kubectl exec --stdin --tty -n database garage-0 -- ./garage layout assign 88d0992a0c66365f -z zero -c 64G;
+kubectl exec --stdin --tty -n database garage-0 -- ./garage layout assign 9ab259b31473b358 -z one -c 64G;
+kubectl exec --stdin --tty -n database garage-0 -- ./garage layout assign 24bfa6d6eb202ed1 -z two -c 64G;
+kubectl exec --stdin --tty -n database garage-0 -- ./garage layout show;
+kubectl exec --stdin --tty -n database garage-0 -- ./garage layout apply --version 1;

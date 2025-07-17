3ea0c93a00e664abb98f6958717b84b8cef999bb
David Driscoll <david.driscoll@gmail.com>
2025-07-17 11:13:40 -0400

    disabled

--- kubernetes/apps/database/garage/boostrap-garage.sh
+++ kubernetes/apps/database/garage/boostrap-garage.sh
@@ -1,6 +1,6 @@
 kubectl exec --stdin --tty -n database garage-0 -- ./garage status
-kubectl exec --stdin --tty -n database garage-0 -- ./garage layout assign ff44f5f7bfd3374d1c76a01acd987a3b8b1e04678fdfaceb6605533339b0ea71 -z sgc -c 64G;
-kubectl exec --stdin --tty -n database garage-0 -- ./garage layout assign 9ab259b31473b358 -z one -c 64G;
-kubectl exec --stdin --tty -n database garage-0 -- ./garage layout assign 24bfa6d6eb202ed1 -z two -c 64G;
+kubectl exec --stdin --tty -n database garage-0 -- ./garage layout assign 3a6b5eac0172d58a4c03514d1e7a546091c60ddc7ea2e8bf655b5760b4be0222 -z sgc -c 64G;
+kubectl exec --stdin --tty -n database garage-0 -- ./garage layout assign 7331c42992d16d73ef5d125b27b04efc5321698e636c19e27f6db68f896629c5 -z sgc -c 64G;
+kubectl exec --stdin --tty -n database garage-0 -- ./garage layout assign 3d7bd531e2853b0f29d8291084c2ecddc5b4eb642c3b2957786983a823603e17 -z sgc -c 64G;
 kubectl exec --stdin --tty -n database garage-0 -- ./garage layout show;
 kubectl exec --stdin --tty -n database garage-0 -- ./garage layout apply --version 1;

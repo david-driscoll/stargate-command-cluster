3f970a3b6195b61241e4cde5308b08f6874585df
David Driscoll <david.driscoll@gmail.com>
2025-07-19 14:22:18 -0400

    shuffle and add mysql cluster, cause why not?

--- kubernetes/apps/database/garage/app/resources/bootstrap-garage.sh
+++ kubernetes/apps/database/garage/app/resources/bootstrap-garage.sh
@@ -48,3 +48,7 @@ else
   echo "Layout already assigned. Skipping assignment."
 fi
 
+# TOKEN=$($GARAGE_CMD admin-token create webui)
+# kubectl create secret generic tailscale-authkey --from-literal=authkey="$authkey" --dry-run=client -o yaml | kubectl apply -f -
+# kubectl annotate secret tailscale-authkey reflector.v1.k8s.emberstack.com/reflection-allowed='true' --dry-run=client -o yaml | kubectl apply -f -
+# kubectl annotate secret tailscale-authkey reloader.stakater.com/auto='true' --dry-run=client -o yaml | kubectl apply -f -

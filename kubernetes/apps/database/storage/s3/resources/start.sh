3e9ee65bb5cede891fa4e86a329395ce465ec067
David Driscoll <david.driscoll@gmail.com>
2025-07-22 22:12:31 -0400

    start script

--- kubernetes/apps/database/storage/s3/resources/start.sh
+++ kubernetes/apps/database/storage/s3/resources/start.sh
@@ -7,4 +7,4 @@ rclone mkdir /data/mysql
 rclone mkdir /data/postgres
 rclone mkdir /data/tivi-cache
 rclone mkdir /data/tivi-results
-rclone serve s3 --cache-dir /cache --vfs-cache-mode writes --auth-key $R3_USER_CLUSTER_USER,$R3_PASSWORD_CLUSTER_USER --auth-key $R3_USER_AUTHENTIK,$R3_PASSWORD_AUTHENTIK --auth-key $R3_USER_BACKREST,$R3_PASSWORD_BACKREST --auth-key $R3_USER_MYSQL,$R3_PASSWORD_MYSQL --auth-key $R3_USER_POSTGRES,$R3_PASSWORD_POSTGRES --auth-key $R3_USER_TIVI_SYNC,$R3_PASSWORD_TIVI_SYNC /data
+rclone serve s3 --cache-dir /cache --vfs-cache-mode writes --addr :8080 --auth-key $R3_USER_CLUSTER_USER,$R3_PASSWORD_CLUSTER_USER --auth-key $R3_USER_AUTHENTIK,$R3_PASSWORD_AUTHENTIK --auth-key $R3_USER_BACKREST,$R3_PASSWORD_BACKREST --auth-key $R3_USER_MYSQL,$R3_PASSWORD_MYSQL --auth-key $R3_USER_POSTGRES,$R3_PASSWORD_POSTGRES --auth-key $R3_USER_TIVI_SYNC,$R3_PASSWORD_TIVI_SYNC /data

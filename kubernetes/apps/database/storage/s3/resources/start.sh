0f27f963580a5015d3c0153562bd72006b1462fa
David Driscoll <david.driscoll@gmail.com>
2025-07-24 11:02:55 -0400

    just us bb as the backend

--- kubernetes/apps/database/storage/s3/resources/start.sh
+++ kubernetes/apps/database/storage/s3/resources/start.sh
@@ -2,10 +2,9 @@
 
 rclone mkdir /data/authentik
 rclone mkdir /data/iris
-rclone mkdir /data/backblaze-keys
 rclone mkdir /data/backrest
 rclone mkdir /data/mysql
 rclone mkdir /data/postgres
 rclone mkdir /data/tivi-cache
 rclone mkdir /data/tivi-results
-rclone serve s3 --cache-dir /cache --vfs-cache-mode writes --addr :8080 --auth-key $R3_USER_CLUSTER_USER,$R3_PASSWORD_CLUSTER_USER --auth-key $R3_USER_AUTHENTIK,$R3_PASSWORD_AUTHENTIK --auth-key $R3_USER_BACKBLAZE_KEYS,$R3_PASSWORD_BACKBLAZE_KEYS --auth-key $R3_USER_BACKREST,$R3_PASSWORD_BACKREST --auth-key $R3_USER_MYSQL,$R3_PASSWORD_MYSQL --auth-key $R3_USER_POSTGRES,$R3_PASSWORD_POSTGRES --auth-key $R3_USER_TIVI_SYNC,$R3_PASSWORD_TIVI_SYNC /data
+rclone serve s3 --cache-dir /cache --vfs-cache-mode writes --addr :8080 --auth-key $R3_USER_CLUSTER_USER,$R3_PASSWORD_CLUSTER_USER --auth-key $R3_USER_AUTHENTIK,$R3_PASSWORD_AUTHENTIK --auth-key $R3_USER_BACKREST,$R3_PASSWORD_BACKREST --auth-key $R3_USER_MYSQL,$R3_PASSWORD_MYSQL --auth-key $R3_USER_POSTGRES,$R3_PASSWORD_POSTGRES --auth-key $R3_USER_TIVI_SYNC,$R3_PASSWORD_TIVI_SYNC /data

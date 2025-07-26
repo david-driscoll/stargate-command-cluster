dbcea13354f7c0c932e341075b5b4adb6d2a869c
David Driscoll <david.driscoll@gmail.com>
2025-07-25 23:45:37 -0400

    Adding backrest script

--- kubernetes/apps/backup/backrest/Update.cs
+++ kubernetes/apps/backup/backrest/Update.cs
@@ -118,9 +118,11 @@ var initScripts = new List<string>()
   if [ ! -f "$CONFIG_PATH" ]; then
     echo "{\"repos\": []}" > $CONFIG_PATH
   fi
-
+  cat /app/data/config.json | jq
   jq ".instance = \"${CLUSTER_CNAME}\"" $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+  cat /app/data/config.json | jq
   jq ".auth.disabled = true" $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+  cat /app/data/config.json | jq
   """,
 };
 foreach (var volume in volsyncVolume)
@@ -128,6 +130,7 @@ foreach (var volume in volsyncVolume)
   initScripts.Add($$$"""
   repo_json=$(jq -n --arg id "{{{volume}}}" --arg uri "/shares/volsync/{{{volume}}}" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
   jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+  cat /app/data/config.json | jq
   """);
 }
 
@@ -135,6 +138,7 @@ initScripts.AddRange([
   """
   # remove any duplicate repos by id
   jq '(.repos | group_by(.id) | map(.[0]))' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+  cat /app/data/config.json | jq
   """
 ]);
 
--- kubernetes/apps/backup/backrest/app/resources/init.sh
+++ kubernetes/apps/backup/backrest/app/resources/init.sh
@@ -7,32 +7,47 @@ TEMP_CONFIG_PATH=/tmp/config.json.tmp
 if [ ! -f "$CONFIG_PATH" ]; then
   echo "{\"repos\": []}" > $CONFIG_PATH
 fi
-
+cat /app/data/config.json | jq
 jq ".instance = \"${CLUSTER_CNAME}\"" $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 jq ".auth.disabled = true" $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "pgadmin" --arg uri "/shares/volsync/pgadmin" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "uptime-kuma" --arg uri "/shares/volsync/uptime-kuma" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "autokuma" --arg uri "/shares/volsync/autokuma" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "authentik" --arg uri "/shares/volsync/authentik" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "matter" --arg uri "/shares/volsync/matter" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "home-assistant" --arg uri "/shares/volsync/home-assistant" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "network-ups-tools" --arg uri "/shares/volsync/network-ups-tools" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "scrypted" --arg uri "/shares/volsync/scrypted" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "plex" --arg uri "/shares/volsync/plex" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "tautulli" --arg uri "/shares/volsync/tautulli" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "backrest" --arg uri "/shares/volsync/backrest" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 repo_json=$(jq -n --arg id "restic-server" --arg uri "/shares/volsync/restic-server" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password}');
 jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq
 # remove any duplicate repos by id
 jq '(.repos | group_by(.id) | map(.[0]))' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp $TEMP_CONFIG_PATH $CONFIG_PATH
+cat /app/data/config.json | jq

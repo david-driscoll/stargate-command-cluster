d1e588a5fe0cc2c858e6d4d3330568c61e10b567
David Driscoll <david.driscoll@gmail.com>
2025-07-26 17:25:49 -0400

    fix patch

--- kubernetes/apps/backup/backrest/Update.cs
+++ kubernetes/apps/backup/backrest/Update.cs
@@ -112,31 +112,25 @@ var initScripts = new List<string>()
   #!/bin/sh
 
   set -e
-  CONFIG_PATH=/app/config.json
-  TEMP_CONFIG_PATH=/tmp/config.json.tmp
+  CONFIG_PATH=./config.json
 
   if [ ! -f "$CONFIG_PATH" ]; then
     echo "{\"repos\": []}" > $CONFIG_PATH
   fi
-  cat $CONFIG_PATH | jq
-  jq ".instance = \"${CLUSTER_CNAME}\"" $CONFIG_PATH > $TEMP_CONFIG_PATH && cp -f $TEMP_CONFIG_PATH $CONFIG_PATH
-  jq ".version = 4" $CONFIG_PATH > $TEMP_CONFIG_PATH && cp -f $TEMP_CONFIG_PATH $CONFIG_PATH
-  jq ".auth.disabled = true" $CONFIG_PATH > $TEMP_CONFIG_PATH && cp -f $TEMP_CONFIG_PATH $CONFIG_PATH
+  cat $CONFIG_PATH | jq ".instance = \"${CLUSTER_CNAME}\"" | jq ".version = 4" | jq ".auth.disabled = true" | tee $CONFIG_PATH | jq
   """,
 };
 foreach (var volume in volsyncVolume)
 {
   initScripts.Add($$$"""
   repo_json=$(jq -n --arg id "{{{volume}}}" --arg uri "/shares/volsync/{{{volume}}}" --arg password "VOLSYNC_PASSWORD" '{id: $id, uri: $uri, password: $password, auto_initialize: true}');
-  jq --argjson repo "$repo_json" '.repos += [$repo]' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp -f $TEMP_CONFIG_PATH $CONFIG_PATH
+  cat $CONFIG_PATH | jq --argjson repo "$repo_json" '.repos += [$repo]' | tee $CONFIG_PATH
   """);
 }
 
 initScripts.AddRange([
   """
-  # remove any duplicate repos by id
-  jq '.repos |= (group_by(.id) | map(.[0]))' $CONFIG_PATH > $TEMP_CONFIG_PATH && cp -f $TEMP_CONFIG_PATH $CONFIG_PATH
-  cat /app/config.json | jq
+  cat $CONFIG_PATH | jq '.repos |= (group_by(.id) | map(.[0]))' | tee $CONFIG_PATH | jq
   """
 ]);
 

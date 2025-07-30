3e05e96ba44dc1bbdcee83c4f24808ad0993cdb3
David Driscoll <david.driscoll@gmail.com>
2025-07-30 15:54:01 -0400

    adding authentik pulumi deployment

--- kubernetes/apps/database/storage/s3/resources/start.sh
+++ kubernetes/apps/database/storage/s3/resources/start.sh
@@ -2,9 +2,10 @@
 
 rclone mkdir /data/authentik
 rclone mkdir /data/iris
+rclone mkdir /data/authentik-apps
 rclone mkdir /data/backrest
 rclone mkdir /data/mysql
 rclone mkdir /data/postgres
 rclone mkdir /data/tivi-cache
 rclone mkdir /data/tivi-results
-rclone serve s3 --cache-dir /cache --vfs-cache-mode writes --addr :8080 --auth-key $R3_USER_CLUSTER_USER,$R3_PASSWORD_CLUSTER_USER --auth-key $R3_USER_AUTHENTIK,$R3_PASSWORD_AUTHENTIK --auth-key $R3_USER_BACKREST,$R3_PASSWORD_BACKREST --auth-key $R3_USER_MYSQL,$R3_PASSWORD_MYSQL --auth-key $R3_USER_POSTGRES,$R3_PASSWORD_POSTGRES --auth-key $R3_USER_TIVI_SYNC,$R3_PASSWORD_TIVI_SYNC /data
+rclone serve s3 --cache-dir /cache --vfs-cache-mode writes --addr :8080 --auth-key $R3_USER_CLUSTER_USER,$R3_PASSWORD_CLUSTER_USER --auth-key $R3_USER_AUTHENTIK,$R3_PASSWORD_AUTHENTIK --auth-key $R3_USER_AUTHENTIK_APPS,$R3_PASSWORD_AUTHENTIK_APPS --auth-key $R3_USER_BACKREST,$R3_PASSWORD_BACKREST --auth-key $R3_USER_MYSQL,$R3_PASSWORD_MYSQL --auth-key $R3_USER_POSTGRES,$R3_PASSWORD_POSTGRES --auth-key $R3_USER_TIVI_SYNC,$R3_PASSWORD_TIVI_SYNC /data

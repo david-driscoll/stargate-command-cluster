855f35ef16c43514dd190c54e27a844292c44908
David Driscoll <david.driscoll@gmail.com>
2025-08-15 21:02:49 -0400

    re-enable authentik

--- kubernetes/apps/sgc/idp/pulumi/Models/UptimeKuma/GroupUptime.cs
+++ kubernetes/apps/sgc/idp/pulumi/Models/UptimeKuma/GroupUptime.cs
@@ -4,6 +4,10 @@ namespace applications.Models.UptimeKuma;
 
 public class GroupUptime : UptimeBase
 {
+  public GroupUptime()
+  {
+    this.Interval = 30;
+  }
   public override string Type { get; } = "group";
   [YamlDotNet.Serialization.YamlMember(Alias = "accepted_statuscodes")]
   [System.Text.Json.Serialization.JsonPropertyName("accepted_statuscodes")]

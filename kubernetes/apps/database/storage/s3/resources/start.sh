7cf2eace88ffa679b1742e0d100b5c8c1d9060c9
David Driscoll <david.driscoll@gmail.com>
2025-08-23 21:35:36 -0400

    fixing flux run

--- kubernetes/apps/database/storage/s3/resources/start.sh
+++ kubernetes/apps/database/storage/s3/resources/start.sh
@@ -2,7 +2,6 @@
 
 rclone mkdir /data/iris
 rclone mkdir /data/backrest
-rclone mkdir /data/postgres
 rclone mkdir /data/tivi-cache
 rclone mkdir /data/tivi-results
-rclone serve s3 --cache-dir /cache --vfs-cache-mode writes --addr :8080 --auth-key $R3_USER_CLUSTER_USER,$R3_PASSWORD_CLUSTER_USER --auth-key $R3_USER_AUTHENTIK,$R3_PASSWORD_AUTHENTIK --auth-key $R3_USER_BACKREST,$R3_PASSWORD_BACKREST --auth-key $R3_USER_POSTGRES,$R3_PASSWORD_POSTGRES --auth-key $R3_USER_TIVI_SYNC,$R3_PASSWORD_TIVI_SYNC /data
+rclone serve s3 --cache-dir /cache --vfs-cache-mode writes --addr :8080 --auth-key $R3_USER_CLUSTER_USER,$R3_PASSWORD_CLUSTER_USER --auth-key $R3_USER_AUTHENTIK,$R3_PASSWORD_AUTHENTIK --auth-key $R3_USER_BACKREST,$R3_PASSWORD_BACKREST --auth-key $R3_USER_TIVI_SYNC,$R3_PASSWORD_TIVI_SYNC /data

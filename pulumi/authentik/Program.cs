f0f9f2fb800f8fe82eb162f041184c8f3a05620f
David Driscoll <david.driscoll@gmail.com>
2025-08-28 13:45:02 -0400

    sync n8n idea?

--- pulumi/authentik/Program.cs
+++ pulumi/authentik/Program.cs
@@ -10,7 +10,11 @@ using models;
 using models.Applications;
 using Pulumi;
 using Pulumi.Authentik;
+using Rocket.Surgery.OnePasswordNativeUnofficial;
 using Config = Pulumi.Config;
+using Pulumi.N8n;
+using Pulumi.N8n.Inputs;
+using UserArgs = Pulumi.N8n.UserArgs;
 
 KubernetesJson.AddJsonOptions(options => { options.Converters.Add(new YamlMemberConverterFactory()); });
 
@@ -34,7 +38,8 @@ return await Deployment.RunAsync(async () =>
 
     if (OperatingSystem.IsLinux())
     {
-      cluster = new Kubernetes(await KubernetesClientConfiguration.BuildConfigFromConfigFileAsync(new MemoryStream(Encoding.UTF8.GetBytes(new Config("kubernetes").Require("kubeconfig")))));
+      cluster = new Kubernetes(await KubernetesClientConfiguration.BuildConfigFromConfigFileAsync(
+        new MemoryStream(Encoding.UTF8.GetBytes(new Config("kubernetes").Require("kubeconfig")))));
     }
     else
     {
@@ -46,9 +51,12 @@ return await Deployment.RunAsync(async () =>
   var groups = new AuthentikGroups();
   var onePasswordProvider = new Rocket.Surgery.OnePasswordNativeUnofficial.Provider("onepassword", new()
   {
-    Vault = Environment.GetEnvironmentVariable("CONNECT_VAULT") ?? throw new InvalidOperationException("CONNECT_VAULT is not set"),
-    ConnectHost = Environment.GetEnvironmentVariable("CONNECT_HOST") ?? throw new InvalidOperationException("CONNECT_HOST is not set"),
-    ConnectToken = Environment.GetEnvironmentVariable("CONNECT_TOKEN") ?? throw new InvalidOperationException("CONNECT_TOKEN is not set"),
+    Vault = Environment.GetEnvironmentVariable("CONNECT_VAULT") ??
+            throw new InvalidOperationException("CONNECT_VAULT is not set"),
+    ConnectHost = Environment.GetEnvironmentVariable("CONNECT_HOST") ??
+                  throw new InvalidOperationException("CONNECT_HOST is not set"),
+    ConnectToken = Environment.GetEnvironmentVariable("CONNECT_TOKEN") ??
+                   throw new InvalidOperationException("CONNECT_TOKEN is not set"),
   });
 
   var flows = Flows2.CreateFlows(onePasswordProvider);
@@ -77,7 +85,7 @@ return await Deployment.RunAsync(async () =>
     });
   }
 
-  _ = new AuthentikApplicationResources(new()
+  var appResources = new AuthentikApplicationResources(new()
   {
     OnePasswordProvider = onePasswordProvider,
     Groups = groups,
@@ -86,6 +94,25 @@ return await Deployment.RunAsync(async () =>
     ClusterFlows = clusterFlows,
     PropertyMappings = Flows2.PropertyMappings,
   });
+
+  // var n8nToken =
+  //   GetAPICredential.Invoke(
+  //     new GetAPICredentialInvokeArgs() { Id = "d7it3mahq5itdb32ezbtmelube", Title = "N8N Api Key", Vault = "Eris" },
+  //     new InvokeOptions() { Provider = onePasswordProvider });
+  // var n8nProvider = new Pulumi.N8n.Provider("n8n", new Pulumi.N8n.ProviderArgs()
+  // {
+  //   ApiKey = n8nToken.Apply(z => z.Credential),
+  //   BaseUrl = Output.Format($"https://{n8nToken.Apply(z => z.Hostname)}"),
+  // });
+  //
+  // GetUsers.Invoke(new() { IsActive = true })
+  //   .Apply(z => z.Users.Select(user => new Pulumi.N8n.User(user.Name, new UserArgs()
+  //     {
+  //       Email = user.Email,
+  //       FirstName = user.Name,
+  //     }, new() { Parent = appResources, Provider = n8nProvider })
+  //   ));
+  // ;
 
   // Export outputs here
   return new Dictionary<string, object?>();

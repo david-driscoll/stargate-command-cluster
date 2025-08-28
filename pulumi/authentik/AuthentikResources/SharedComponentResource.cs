91a5d4ba44b003fe40feecab738701f8374702ae
David Driscoll <david.driscoll@gmail.com>
2025-08-28 13:45:02 -0400

    sync n8n idea?

--- pulumi/authentik/AuthentikResources/SharedComponentResource.cs
+++ pulumi/authentik/AuthentikResources/SharedComponentResource.cs
@@ -5,7 +5,6 @@ namespace authentik.AuthentikResources;
 public abstract class SharedComponentResource : ComponentResource
 {
   protected CustomResourceOptions _parent;
-  protected readonly ComponentResourceOptions _cparent;
 
   public SharedComponentResource(string type, string name, ComponentResourceOptions? options = null) : base(type, name,
     options)

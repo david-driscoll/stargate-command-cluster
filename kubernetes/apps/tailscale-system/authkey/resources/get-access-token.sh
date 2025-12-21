5d5b48ffb660ce90ca0b3db0350475d5804cdaef
David Driscoll <david.driscoll@gmail.com>
2025-12-21 00:34:06 -0500

    Revert "enabled tailscale"
    This reverts commit b81917764151f12987c76ac6e32877c1481442a7.

---
+++ kubernetes/apps/tailscale-system/authkey/resources/get-access-token.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y curl jq
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+./kubectl version
+
+authkey=$(go run tailscale.com/cmd/get-authkey@latest -ephemeral -reusable -preauth -tags "tag:${CLUSTER_CNAME}")
+
+echo "authkey: $authkey"
+
+token_response=$(curl -d "client_id=$TS_API_CLIENT_ID" -d "client_secret=$TS_API_CLIENT_SECRET" "https://api.tailscale.com/api/v2/oauth/token")
+access_token=$(echo $token_response | jq -r '.access_token')
+echo "access_token: $access_token"
+
+./kubectl create secret generic access-token --from-literal=token="$access_token" --dry-run=client -o yaml | ./kubectl apply -f -
+./kubectl annotate secret access-token reflector.v1.k8s.emberstack.com/reflection-allowed='true' --dry-run=client -o yaml | ./kubectl apply -f -
+./kubectl annotate secret access-token reloader.stakater.com/auto='true' --dry-run=client -o yaml | ./kubectl apply -f -
+
+./kubectl create secret generic authkey --from-literal=authkey="$authkey" --dry-run=client -o yaml | ./kubectl apply -f -
+./kubectl annotate secret authkey reflector.v1.k8s.emberstack.com/reflection-allowed='true' --dry-run=client -o yaml | ./kubectl apply -f -
+./kubectl annotate secret authkey reloader.stakater.com/auto='true' --dry-run=client -o yaml | ./kubectl apply -f -

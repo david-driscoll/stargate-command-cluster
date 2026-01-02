9f51e936533e8c168e410fbd72745d7d5ecf134b
David Driscoll <david.driscoll@gmail.com>
2026-01-01 21:29:23 -0500

    updated github token job maybe?

--- kubernetes/apps/kube-system/github-token/resources/refresh-token.sh
+++ kubernetes/apps/kube-system/github-token/resources/refresh-token.sh
@@ -13,17 +13,17 @@ echo "Generating GitHub access token for App ID $GITHUB_APP_ID and Installation 
 
 ACCESS_TOKEN="$(go run github.com/slawekzachcial/gha-token@latest --appId $GITHUB_APP_ID --keyPath $KEY_PATH --installId $GITHUB_APP_INSTALLATION_ID)"
 
-token_b64=$(printf '%s' "${ACCESS_TOKEN}" | base64 | tr -d '\n')
-service_account_token=$(cat "${SA_TOKEN_PATH}")
+token_b64=$(printf '%s' "$ACCESS_TOKEN" | base64 | tr -d '\n')
+service_account_token=$(cat "$SA_TOKEN_PATH")
 
 PATCH_PAYLOAD="{\"metadata\":{\"annotations\":{\"reloader.stakater.com/auto\":\"true\",\"reflector.v1.k8s.emberstack.com/reflection-allowed\":\"true\",\"reflector.v1.k8s.emberstack.com/reflection-auto-enabled\":\"true\"}},\"data\":{\"token\":\"${token_b64}\"}}"
 
 curl -sSf \
-  --cacert "${CA_CERT_PATH}" \
-  -H "Authorization: Bearer ${service_account_token}" \
+  --cacert "$CA_CERT_PATH" \
+  -H "Authorization: Bearer $service_account_token" \
   -H "Content-Type: application/merge-patch+json" \
   -X PATCH \
-  -d "${PATCH_PAYLOAD}" \
-  "https://kubernetes.default.svc.cluster.local/api/v1/namespaces/flux-system/secrets/${SECRET_NAME}"
+  -d "$PATCH_PAYLOAD" \
+  "https://kubernetes.default.svc.cluster.local/api/v1/namespaces/flux-system/secrets/$SECRET_NAME"
 
 echo "Successfully updated GitHub token secret"
